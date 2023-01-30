0e47d807f3f9c88c2d7907db44c9453dacf417f1
nicompiledev <nsrivera80@misena.edu.co>
2023-01-29 21:21:49 -0500

    added CardDeal, Clients, FeedbackCard, Footer and Testimonials components.

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { apple, bill, google } from "../assets";
+import styles, { layout } from "../style";
 
-function Billing() {
-  return (
-    <div>Billing</div>
-  )
-}
+const Billing = () => (
+  <section id="product" className={layout.sectionReverse}>
+    <div className={layout.sectionImgReverse}>
+      <img src={bill} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
 
-export default Billing
+      {/* gradient start */}
+      <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+      <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient" />
+      {/* gradient end */}
+    </div>
+
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Easily control your <br className="sm:block hidden" /> billing &
+        invoicing
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+      Easily controlling your billing and invoicing can greatly streamline your financial processes and help you keep track of your expenses with ease. With a clear and efficient system in place, you can save time and reduce the risk of errors or miscommunications. Whether you are a small business owner or managing finances for a large corporation, having a reliable billing and invoicing system is essential for maintaining a healthy financial situation. By streamlining these processes, you can focus on growing your business and achieving your goals.
+
+
+
+
+      </p>
+
+      <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+        <img src={apple} alt="google_play" className="w-[128.86px] h-[42.05px] object-contain mr-5 cursor-pointer" />
+        <img src={google} alt="google_play" className="w-[144.17px] h-[43.08px] object-contain cursor-pointer" />
+      </div>
+    </div>
+  </section>
+);
+
+export default Billing;
--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
 
-function CardDeal() {
-  return (
-    <div>CardDeal</div>
-  )
-}
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className="sm:block hidden" /> in few easy
+        steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+      Finding the perfect credit card can be a hassle, but with just a few easy steps, you can find a better deal that fits your needs. By taking the time to compare offers and carefully consider your spending habits, you can ensure that you're getting the most value for your money.
+      </p>
 
-export default CardDeal
+      <Button styles={`mt-10`} />
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="billing" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+);
+
+export default CardDeal;
